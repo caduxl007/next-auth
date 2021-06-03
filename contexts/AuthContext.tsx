96dce12356dc03086a5d878525e1e7d1c1d51781
caduxl007 <eduardo2020dev@gmail.com>
2021-06-03 19:24:56 -0300

    logout broadcast

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -23,6 +23,7 @@ type SignInCredentials = {
 
 type AuthContextData = {
   signIn(credentials: SignInCredentials): Promise<void>;
+  signOut: () => void;
   user: User;
   isAuthenticated: boolean;
 };
@@ -33,9 +34,13 @@ type AuthProviderProps = {
 
 const AuthContext = createContext({} as AuthContextData);
 
+let authChannel: BroadcastChannel;
+
 export function signOut() {
   destroyCookie(undefined, "nextauth.token");
   destroyCookie(undefined, "nextauth.refreshToken");
+
+  authChannel.postMessage("signOut");
 
   Router.push("/");
 }
@@ -43,6 +48,23 @@ export function signOut() {
 export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>();
   const isAuthenticated = !!user;
+
+  useEffect(() => {
+    authChannel = new BroadcastChannel("auth");
+
+    authChannel.onmessage = (message) => {
+      switch (message.data) {
+        case "signOut":
+          signOut();
+          break;
+        case "signIn":
+          Router.push("/dashboard");
+          break;
+        default:
+          break;
+      }
+    };
+  }, []);
 
   useEffect(() => {
     const { "nextauth.token": token } = parseCookies();
@@ -93,13 +115,15 @@ export function AuthProvider({ children }: AuthProviderProps) {
       api.defaults.headers["Authorization"] = `Bearer ${token}`;
 
       Router.push("/dashboard");
+
+      authChannel.postMessage("signIn");
     } catch (err) {
       console.log(err);
     }
   }
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, user, signIn }}>
+    <AuthContext.Provider value={{ isAuthenticated, user, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
--- pages/metrics.tsx
+++ pages/metrics.tsx
@@ -2,22 +2,20 @@ import { useAuth } from "../contexts/AuthContext";
 import { setupAPIClient } from "../services/api";
 import { withSSRAuth } from "../utils/withSSRAuth";
 
-export default function Dashboard() {
-  const { user, signOut } = useAuth();
+export default function Metrics() {
+  const { user } = useAuth();
 
-  return (
-    <>
-      <button type="button" onClick={signOut}>SignOut</button>
-      <h1>Dashboard: {user?.email}</h1>
-    </>
-  );
+  return <h1>Metrics: {user?.email}</h1>;
 }
 
 export const getServerSideProps = withSSRAuth(async (ctx) => {
   const apiClient = setupAPIClient(ctx);
-  const response = await apiClient.get("/me");
+  const response  = await apiClient.get('/me');
 
   return {
     props: {},
   };
+}, {
+  permissions: ['metrics.list'],
+  roles: ['administrator']
 });
